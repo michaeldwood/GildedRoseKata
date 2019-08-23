5fb03d6a1e685c545ce082f768af5a7404aa0ed1
Michael Wood <michaelwood@elliswhittam.com>
2019-08-23 22:00:31 +0100

    Added test cases

---
+++ Tests/GildedRoseTest.cs
@@ -0,0 +1,104 @@
+﻿using GildedRose.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRose.Tests
+{
+    [TestFixture]
+    class GildedRoseTest
+    {
+        [Test]
+        public void AgedBrie()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 1, Quality = 1 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateInventory();
+            Assert.AreEqual(0, Items[0].SellIn);
+            Assert.AreEqual(2, Items[0].Quality);
+        }
+
+        [Test]
+        public void BackStagePass()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes", SellIn = -1, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateInventory();
+            Assert.AreEqual(-2, Items[0].SellIn);
+            Assert.AreEqual(0, Items[0].Quality);
+        }
+
+        [Test]
+        public void BackStagePass2()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes", SellIn = 9, Quality = 2 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateInventory();
+            Assert.AreEqual(8, Items[0].SellIn);
+            Assert.AreEqual(4, Items[0].Quality);
+        }
+
+        [Test]
+        public void Sulfuras()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras", SellIn = 2, Quality = 2 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateInventory();
+            Assert.AreEqual(2, Items[0].SellIn);
+            Assert.AreEqual(2, Items[0].Quality);
+        }
+
+        [Test]
+        public void NormalItem()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Normal Item", SellIn = -1, Quality = 55 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateInventory();
+            Assert.AreEqual(-2, Items[0].SellIn);
+            Assert.AreEqual(50, Items[0].Quality);
+        }
+
+        [Test]
+        public void NormalItem2()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Normal Item", SellIn = 2, Quality = 2 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateInventory();
+            Assert.AreEqual(1, Items[0].SellIn);
+            Assert.AreEqual(1, Items[0].Quality);
+        }
+
+        [Test]
+        public void InvalidItem()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "INVALID ITEM", SellIn = 2, Quality = 2 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateInventory();
+            Assert.AreEqual("NO SUCH ITEM", Items[0].Name);
+        }
+
+        [Test]
+        public void Conjured()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Conjured", SellIn = 2, Quality = 2 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateInventory();
+            Assert.AreEqual(1, Items[0].SellIn);
+            Assert.AreEqual(0, Items[0].Quality);
+        }
+
+        [Test]
+        public void Conjured2()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Conjured", SellIn = -1, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateInventory();
+            Assert.AreEqual(-2, Items[0].SellIn);
+            Assert.AreEqual(1, Items[0].Quality);
+        }
+
+    }
+}
